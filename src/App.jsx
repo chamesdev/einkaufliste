cbef860b9c75849a88eb9e2bef5fbf1fac4ee94d
james <james.bennett@swisscom.com>
2026-09-20 15:26:08 +0200

    Refactor App component to manage items state and add item functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import "./App.css";
+import { useState } from "react";
 import Item from "./Item";
-
-const items = [{ name: "Banana" }, { name: "Apple" }, { name: "Orange" }];
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    { name: "Banana" },
+    { name: "Apple" },
+    { name: "Orange" },
+  ]);
+  const [item, setItem] = useState({ name: "" });
+
   return (
     <main className="app">
       <h1>Einkaufsliste</h1>
+      <h2>Item Hinzufügen</h2>
+      <div>
+        <input
+          type="text"
+          value={item.name}
+          onChange={(e) => setItem({ name: e.target.value })}
+        />
+        <button
+          onClick={() => {
+            if (item.name.trim() !== "") {
+              setItems([...items, item]);
+              setItem({ name: "" });
+            }
+          }}
+        >
+          Hinzufügen
+        </button>
+      </div>
       <h2>{items.length} Artikel</h2>
       <div>
         {items.map((item) => (
